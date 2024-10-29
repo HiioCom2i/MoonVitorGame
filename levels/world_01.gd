fdbae2b2471de7cd7ffa83d5227eb748cd5cb056
DaviGibson <davigibson10@gmail.com>
2024-10-29 12:44:34 -0300

    ataque coletável c animação adicionado

--- levels/world_01.gd
+++ levels/world_01.gd
@@ -2,17 +2,27 @@ extends Node2D
 
 
 var lifes_collected = 0 
+var attacks_collected = 0
 
 func _ready():
-	# Suponha que você instanciou as moedas dinamicamente ou já tem elas na cena
-	# Conecte o sinal de cada moeda ao método que atualiza o contador
-	for vidas in $vidas.get_children():  # Suponha que as moedas estão no grupo "Coins"
+	# Conectando o sinal de cada vida ao método que atualiza seu contador
+	for vidas in $vidas.get_children():  # Todas vidas estão no Node2D 'vidas'
 		vidas.connect("life_collected", Callable(self, "_on_life_collected"))
+	# Conectando o sinal de cada ataque ao método que atualiza seu contador
+	for ataque in $ataques.get_children():  # Todos ataques estão no Node2D 'ataques'
+		ataque.connect("attack_collected", Callable(self, "_on_attack_collected"))
+		print("sinal de ataque conectado")
 		
 func _on_life_collected():
 	lifes_collected += 1
 	print("vidas coletadas " + str(lifes_collected))
 	$HUD/container_vida/Controle2/contador_vidas.text = " x " + str(lifes_collected)
+
+func _on_attack_collected():
+	attacks_collected += 1
+	print("ataques coletado " + str(attacks_collected))
+	$HUD/container_ataque/Controle2/contador_ataques.text = " x " + str(attacks_collected)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
